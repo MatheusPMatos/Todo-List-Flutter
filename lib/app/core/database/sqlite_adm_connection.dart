93f042938e5d56423aa458885aa2b2507f650e3f
MatheusPMatos <mpeima@yahoo.com.br>
2025-08-13 18:58:37 -0300

    ajsute projeto flutter

--- lib/app/core/database/sqlite_adm_connection.dart
+++ lib/app/core/database/sqlite_adm_connection.dart
@@ -1,25 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:todolist_provider/app/core/database/sqlite_conection_factory.dart';
 
-class SqliteAdmConnection  with WidgetsBindingObserver {
-
+class SqliteAdmConnection with WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-
     final connection = SqliteConectionFactory();
-   
-   switch(state){
 
-    case AppLifecycleState.resumed:
-    break;
-    case AppLifecycleState.inactive:
-    case AppLifecycleState.paused:
-    case AppLifecycleState.detached:
-      connection.closeConnection();
-    break;
-   }
+    switch (state) {
+      case AppLifecycleState.resumed:
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.detached:
+        connection.closeConnection();
+        break;
+    }
     super.didChangeAppLifecycleState(state);
   }
-  
 }
--- lib/app/core/validators/validators.dart
+++ lib/app/core/validators/validators.dart
@@ -1,17 +1,15 @@
-
 import 'package:flutter/material.dart';
 
 class Validators {
-  
   Validators._();
 
-  static FormFieldValidator compare(TextEditingController? valueEC, String message){
-    return(value){
+  static FormFieldValidator compare(
+      TextEditingController? valueEC, String message) {
+    return (value) {
       final valueCompare = valueEC?.text ?? '';
-      if(value == null || (value!= null && value != valueCompare)){
+      if (value == null || (value != null && value != valueCompare)) {
         return message;
       }
-      
     };
   }
 }
